80f8b4e3a36c8a1f15711eff6f78a01f861dde24
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-02 13:35:59 -0400

    feat: initialize CTRT domain package

---
+++ src/ctrt/__init__.py
@@ -0,0 +1,26 @@
+"""CTRT constitutional domain contracts.
+
+This package intentionally contains no machine-learning implementation during Phase 0.
+"""
+
+from ctrt.contracts import (
+    Analyzer,
+    AnalyzerIdentity,
+    ContentItem,
+    EvidenceSpan,
+    ModelResult,
+    NormalizedScore,
+    ResultStatus,
+    SourceType,
+)
+
+__all__ = [
+    "Analyzer",
+    "AnalyzerIdentity",
+    "ContentItem",
+    "EvidenceSpan",
+    "ModelResult",
+    "NormalizedScore",
+    "ResultStatus",
+    "SourceType",
+]
